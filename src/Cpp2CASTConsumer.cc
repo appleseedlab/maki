17c57fb862301847313ab09f14d22ad36f488990
Brent Pappas <pappasbrent@gmail.com>
2022-09-16 20:31:21 -0400

    collect alignment info on stmt/decl/typeloc macros
    this is a _very_ rough draft, but gets the idea across

--- src/Cpp2CASTConsumer.cc
+++ src/Cpp2CASTConsumer.cc
@@ -1,9 +1,45 @@
 #include "Cpp2CASTConsumer.hh"
+#include "ExpansionArgumentMatchHandler.hh"
+#include "ExpansionMatchHandler.hh"
+#include "ExpansionMatcher.hh"
+#include "DeclStmtTypeLoc.hh"
 
 #include "clang/Lex/Preprocessor.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
 
 namespace cpp2c
 {
+
+    DeclStmtTypeLoc *firstASTRootExpandsToAlignWithRange(
+        clang::SourceManager &SM,
+        std::vector<DeclStmtTypeLoc> &ASTRoots,
+        clang::SourceRange Range)
+    {
+
+        // TODO: Maybe instead we should align the expansion with the root
+        // that has the most inclusive range?
+        for (auto &&R : ASTRoots)
+            if (Range == SM.getExpansionRange(R.getSourceRange()).getAsRange())
+                return &R;
+        return nullptr;
+    }
+
+    DeclStmtTypeLoc *firstASTRootSpelledInRange(
+        clang::SourceManager &SM,
+        std::vector<DeclStmtTypeLoc> &ASTRoots,
+        clang::SourceRange Range)
+    {
+
+        // TODO: Maybe we should check that spelling location of end loc
+        // is in range instead of begin loc?
+        for (auto &&R : ASTRoots)
+            if (Range.fullyContains(
+                    SM.getSpellingLoc(R.getSourceRange().getBegin())))
+                return &R;
+        return nullptr;
+    }
+
     Cpp2CASTConsumer::Cpp2CASTConsumer(clang::CompilerInstance &CI)
     {
         clang::Preprocessor &PP = CI.getPreprocessor();
@@ -16,9 +52,87 @@ namespace cpp2c
 
     void Cpp2CASTConsumer::HandleTranslationUnit(clang::ASTContext &Ctx)
     {
-        for (auto TopLevelExpansion : MF->TopLevelExpansions)
+        auto &SM = Ctx.getSourceManager();
+        for (auto TLE : MF->TopLevelExpansions)
         {
-            TopLevelExpansion->dump(llvm::errs());
+            // TLE->dump(llvm::errs());
+            using namespace clang::ast_matchers;
+
+            //// Find potential AST roots of the entire expansion
+
+            // Match stmts
+            MATCH_EXPANSION_ROOTS_OF(stmt, TLE);
+
+            // Match decls
+            MATCH_EXPANSION_ROOTS_OF(decl, TLE);
+
+            // Match type locs
+            MATCH_EXPANSION_ROOTS_OF(typeLoc, TLE);
+
+            llvm::errs() << "Top level expansion of "
+                         << TLE->Name.str()
+                         << "\n";
+            // llvm::errs() << "AST roots:\n";
+            // if (!TLE->ASTRoots.empty())
+            //     for (auto R : TLE->ASTRoots)
+            //         R.dump();
+            // else
+            //     llvm::errs() << "None\n";
+            TLE->AlignedRoot = firstASTRootExpandsToAlignWithRange(SM,
+                                                                   TLE->ASTRoots,
+                                                                   TLE->SpellingRange);
+            llvm::errs() << "Aligned root: \n";
+            if (TLE->AlignedRoot)
+                TLE->AlignedRoot->dump();
+            else
+                llvm::errs() << "None\n";
+
+            //// Find potential AST roots of the expansion's arguments
+            if (!TLE->Arguments.empty())
+            {
+                for (auto &&Arg : TLE->Arguments)
+                {
+
+                    // Match stmts
+                    MATCH_ARGUMENT_ROOTS_OF(stmt, (&Arg));
+
+                    // Match decls
+                    MATCH_ARGUMENT_ROOTS_OF(decl, (&Arg));
+
+                    // Match type locs
+                    MATCH_ARGUMENT_ROOTS_OF(typeLoc, (&Arg));
+
+                    llvm::errs() << "Number of AST roots for argument "
+                                 << Arg.Name << ": "
+                                 << std::to_string(Arg.ASTRoots.size())
+                                 << "\n";
+
+                    if (!Arg.TokenRanges.empty())
+                    {
+                        clang::SourceRange ArgRange(
+                            Arg.TokenRanges.front().getBegin(),
+                            Arg.TokenRanges.back().getEnd());
+
+                        // llvm::errs() << "Arg range: ";
+                        // ArgRange.dump(SM);
+
+                        Arg.AlignedRoot =
+                            firstASTRootSpelledInRange(SM,
+                                                       Arg.ASTRoots,
+                                                       ArgRange);
+                    }
+                    // Try to find an AST node aligned with this argument
+                    llvm::errs() << "Aligned root for argument "
+                                 << Arg.Name
+                                 << ":\n";
+                    if (Arg.AlignedRoot)
+                        Arg.AlignedRoot->dump();
+                    else
+                        llvm::errs() << "None\n";
+                }
+            }
+            else
+                llvm::errs() << "No arguments\n";
         }
     }
 } // namespace cpp2c
--- src/ExpansionMatcher.hh
+++ src/ExpansionMatcher.hh
@@ -6,42 +6,86 @@ namespace cpp2c
 {
     using namespace clang::ast_matchers;
 
-    // Matches the root AST nodes of a given macro expansion.
+    // Matches all AST nodes who expand to a source range that shares
+    // begin/end with the given range
     AST_POLYMORPHIC_MATCHER_P2(
-        isMacroExpansionASTRoot,
+        expandsToShareSourceRangeEdge,
         AST_POLYMORPHIC_SUPPORTED_TYPES(clang::Decl,
                                         clang::Stmt,
                                         clang::TypeLoc),
         clang::ASTContext *, Ctx,
-        MacroExpansionNode *, Expansion)
+        clang::SourceRange, Range)
     {
         auto &SM = Ctx->getSourceManager();
-        // auto EB = Expansion->SpellingRange.getBegin();
-        auto EE = Expansion->SpellingRange.getEnd();
-        // auto NB = SM.getExpansionLoc(Node.getBeginLoc());
+        auto EB = Range.getBegin();
+        auto EE = Range.getEnd();
+        auto NB = SM.getExpansionLoc(Node.getBeginLoc());
         auto NE = SM.getExpansionLoc(Node.getEndLoc());
 
-        // If any of this AST node's parents were expanded
-        // from this macro, then this AST node can't be the AST root
-        // for this expansion
-        for (auto Parent : Ctx->getParents(Node))
-            // Confusing syntax for template get
-            if (auto P = Parent.template get<clang::Decl>())
-            {
-                if (SM.getExpansionLoc(P->getEndLoc()) == EE)
-                    return false;
-            }
-            else if (auto P = Parent.template get<clang::Stmt>())
-            {
-                if (SM.getExpansionLoc(P->getEndLoc()) == EE)
-                    return false;
-            }
-            else if (auto P = Parent.template get<clang::TypeLoc>())
-                if (SM.getExpansionLoc(P->getEndLoc()) == EE)
-                    return false;
-
-        // Ensure that this AST node is in fact expanded
-        // from the given expansion
-        return (EE == NE);
+        // Check whether this AST node's beginning or ending loc
+        // matches that of the spelling range's
+        return (EB == NB) || (EE == NE);
     }
+
+    AST_POLYMORPHIC_MATCHER_P2(
+        spellsToShareSourceRangeEdge,
+        AST_POLYMORPHIC_SUPPORTED_TYPES(clang::Decl,
+                                        clang::Stmt,
+                                        clang::TypeLoc),
+        clang::ASTContext *, Ctx,
+        clang::SourceRange, Range)
+    {
+        auto &SM = Ctx->getSourceManager();
+        auto EB = Range.getBegin();
+        auto EE = Range.getEnd();
+        auto NB = SM.getSpellingLoc(Node.getBeginLoc());
+        auto NE = SM.getSpellingLoc(Node.getEndLoc());
+
+        // Check whether this AST node's beginning or ending loc
+        // matches that of the spelling range's
+        return (EB == NB) || (EE == NE);
+    }
+
+// TODO: Instead of having two difference callback classes for
+// expansions and arguments, we should just have one that collects
+// its results in a vec, and then has a method for accessing those results
+
+// Utility macro for matching AST roots of an expansion
+// TODO: Change this to a method for MacroExpansionNode
+#define MATCH_EXPANSION_ROOTS_OF(MATCHER, EXPANSION)          \
+    do                                                        \
+    {                                                         \
+        MatchFinder Finder;                                   \
+        ExpansionMatchHandler Handler(EXPANSION);             \
+        auto Matcher = MATCHER(expandsToShareSourceRangeEdge( \
+                                   &Ctx,                      \
+                                   EXPANSION->SpellingRange)) \
+                           .bind("root");                     \
+        Finder.addMatcher(Matcher, &Handler);                 \
+        Finder.matchAST(Ctx);                                 \
+    } while (0)
+
+// TODO: The following macro doesn't quite work
+// I think for matching arguments, we need to check expansion range edges,
+// not spelling range edges.
+// Yes, actually now that I think about it that makes sense.
+// I should also spend some time thinking of a more general abstraction as well
+#define MATCH_ARGUMENT_ROOTS_OF(MATCHER, ARGUMENT)               \
+    do                                                           \
+    {                                                            \
+        MatchFinder Finder;                                      \
+        ExpansionArgumentMatchHandler Handler(ARGUMENT);         \
+        auto B = (ARGUMENT)->TokenRanges.front().getBegin();     \
+        auto E = (ARGUMENT)->TokenRanges.back().getEnd();        \
+        clang::SourceRange Range(B, E);                          \
+        if (!((ARGUMENT)->TokenRanges.empty()))                  \
+        {                                                        \
+            auto Matcher = MATCHER(spellsToShareSourceRangeEdge( \
+                                       &Ctx,                     \
+                                       Range))                   \
+                               .bind("root");                    \
+            Finder.addMatcher(Matcher, &Handler);                \
+            Finder.matchAST(Ctx);                                \
+        }                                                        \
+    } while (0)
 } // namespace clang::cpp2c
