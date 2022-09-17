68c007fe901e060fde67607e044fd2df9d43eb8b
Brent Pappas <pappasbrent@gmail.com>
2022-09-17 15:11:57 -0400

    add tests for conditional evaluation

---
+++ tests/logical_and.c
@@ -0,0 +1,7 @@
+#define A_AND_B(A, B) ((A) && (B))
+
+int main(int argc, char const *argv[])
+{
+    A_AND_B(1, 2);
+    return 0;
+}
---
+++ tests/logical_or.c
@@ -0,0 +1,7 @@
+#define A_OR_B(A, B) ((A) || (B))
+
+int main(int argc, char const *argv[])
+{
+    A_OR_B(1, 2);
+    return 0;
+}
