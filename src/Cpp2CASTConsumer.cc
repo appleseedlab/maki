3f62d40a58bc63df16891a3cfabd975cbb6939cc
Brent Pappas <pappasbrent@gmail.com>
2022-09-18 15:38:05 -0400

    add checks for hygiene, parameter side-effect freedom, and l-value independence

--- src/Cpp2CASTConsumer.cc
+++ src/Cpp2CASTConsumer.cc
@@ -1,7 +1,8 @@
 #include "Cpp2CASTConsumer.hh"
+#include "DeclStmtTypeLoc.hh"
 #include "ExpansionMatchHandler.hh"
 #include "ExpansionMatcher.hh"
-#include "DeclStmtTypeLoc.hh"
+#include "StmtCollectorMatchHandler.hh"
 
 #include "clang/Lex/Lexer.h"
 #include "clang/Lex/Preprocessor.h"
@@ -10,6 +11,12 @@
 
 #include <algorithm>
 #include <functional>
+
+#include "assert.h"
+
+// TODO:    Remember to check if we should be ignoring implicit casts
+//          and if we should be using TK_IgnoreUnlessSpelledInSource
+//          and ignoringImplicit
 
 namespace cpp2c
 {
@@ -67,10 +74,193 @@ namespace cpp2c
         };
     }
 
+    inline std::function<bool(MacroExpansionArgument)>
+    locInArg(clang::SourceLocation L)
+    {
+        return [L](MacroExpansionArgument Arg)
+        {
+            if (Arg.Tokens.empty())
+                return false;
+            clang::SourceRange ArgTokRange(
+                Arg.Tokens.front().getLocation(),
+                Arg.Tokens.back().getEndLoc());
+            return ArgTokRange.fullyContains(L);
+        };
+    }
+
+    template <typename T>
+    std::vector<const clang::Stmt *>
+    matchAndCollectStmtsIn(
+        clang::ASTContext &Ctx,
+        T *Node,
+        const clang::ast_matchers::internal::BindableMatcher<clang::Stmt> m)
+    {
+        using namespace clang::ast_matchers;
+        MatchFinder Finder;
+        StmtCollectorMatchHandler Handler;
+
+        // We have to add two matchers because by default,
+        // clang appears to not recursively match on AST nodes when given
+        // a specific node to look for matches in
+
+        // Matcher for subtrees
+        auto SubtreeMatcher = m;
+        // Matcher for recursively applying the subtree matcher to the root node
+        auto RootMatcher = stmt(forEachDescendant(SubtreeMatcher));
+
+        // According to Dietrich, the order in which we apply
+        // these matchers is important, but I'm not sure why.
+        Finder.addMatcher(SubtreeMatcher, &Handler);
+        Finder.addMatcher(RootMatcher, &Handler);
+        Finder.match(*Node, Ctx);
+        return Handler.Stmts;
+    }
+
+    // Checks that a given expansion is hygienic.
+    // By hygienic, we mean that it satisfies Clinger and Rees'
+    // strong hygiene condition for macros:
+    // Local variables in the expansion must have been passed as arguments,
+    // and the expansion must not create new declarations that can be
+    // accessed outside of the expansion.
+    // TODO: Check for new declarations!
+    //       Right now we only check for unbound local vars.
+    bool isHygienic(
+        clang::ASTContext &Ctx,
+        cpp2c::MacroExpansionNode *Expansion)
+    {
+        assert(Expansion->AlignedRoot &&
+               Expansion->AlignedRoot->ST &&
+               "Expansion must have an aligned Stmt root");
+        // 1. Collect all local vars in the expansion
+        std::vector<const clang::DeclRefExpr *> LocalVars;
+        for (auto ST : matchAndCollectStmtsIn<const clang::Stmt>(
+                 Ctx,
+                 Expansion->AlignedRoot->ST,
+                 stmt(unless(implicitCastExpr()),
+                      declRefExpr(to(varDecl(hasLocalStorage())))
+                          .bind("root"))))
+            if (auto DRE = clang::dyn_cast<clang::DeclRefExpr>(ST))
+                LocalVars.push_back(DRE);
+            else
+                assert(!"Matched a non DeclRefExpr");
+
+        // 2. Check if any local vars were not passed as arguments.
+        //    If a local var was spelled inside any of the macro's
+        //    arguments, then its hygienic; otherwise, it's not.
+        auto &SM = Ctx.getSourceManager();
+        for (auto DRE : LocalVars)
+        {
+            // NOTE: Maybe we should use endLoc here?
+            auto L = SM.getSpellingLoc(DRE->getBeginLoc());
+            if (!std::any_of(Expansion->Arguments.begin(),
+                             Expansion->Arguments.end(),
+                             locInArg(L)))
+                return false;
+        }
+        // 3. TODO: Check if the expansion contains a decl that could be
+        //          accessed after the macro is expanded.
+        //          (i.e., if the expansion contains a decl that is not
+        //           inside a compound stmt).
+        return true;
+    }
+
+    bool isParameterSideEffectFree(
+        clang::ASTContext &Ctx,
+        MacroExpansionNode *Expansion)
+    {
+        assert(Expansion->AlignedRoot &&
+               Expansion->AlignedRoot->ST &&
+               "Expansion must have an aligned Stmt root");
+        // 1. Collect all expressions with side-effects in the expansion
+        std::vector<const clang::Expr *> SideEffectExprs;
+        for (auto ST : matchAndCollectStmtsIn<const clang::Stmt>(
+                 Ctx,
+                 Expansion->AlignedRoot->ST,
+                 stmt(unless(implicitCastExpr()),
+                      anyOf(
+                          binaryOperator(isAssignmentOperator()).bind("root"),
+                          unaryOperator(hasOperatorName("++")).bind("root"),
+                          unaryOperator(hasOperatorName("--")).bind("root")))))
+            if (auto Ex = clang::dyn_cast<clang::Expr>(ST))
+                SideEffectExprs.push_back(Ex);
+            else
+                assert(!"Matched a non Expr");
+
+        // 2. Check that none of these side-effect exprs came from
+        //    a macro argument
+        auto &SM = Ctx.getSourceManager();
+        return !(std::any_of(SideEffectExprs.begin(),
+                             SideEffectExprs.end(),
+                             [&SM, &Expansion](const clang::Expr *Ex)
+                             {
+                                 auto L = SM.getSpellingLoc(Ex->getBeginLoc());
+                                 return std::any_of(
+                                     Expansion->Arguments.begin(),
+                                     Expansion->Arguments.end(),
+                                     locInArg(L));
+                             }));
+    }
+
+    bool isLValueIndependent(
+        clang::ASTContext &Ctx,
+        MacroExpansionNode *Expansion)
+    {
+        // 1. Collect & exprs and side-effecting exprs
+        std::vector<const clang::Expr *> LValueExprs;
+        for (auto ST : matchAndCollectStmtsIn<const clang::Stmt>(
+                 Ctx,
+                 Expansion->AlignedRoot->ST,
+                 stmt(unless(implicitCastExpr()),
+                      anyOf(
+                          unaryOperator(hasOperatorName("&")).bind("root"),
+                          binaryOperator(isAssignmentOperator()).bind("root"),
+                          unaryOperator(hasOperatorName("++")).bind("root"),
+                          unaryOperator(hasOperatorName("--")).bind("root")))))
+            if (auto Ex = clang::dyn_cast<clang::Expr>(ST))
+                LValueExprs.push_back(Ex);
+            else
+                assert(!"Matched a non Expr");
+
+        // 2. Check that either the entire expr came from an argument,
+        //    or the expr's operand did not come from an argument
+        // TODO: clean this up
+        auto &SM = Ctx.getSourceManager();
+        for (auto Ex : LValueExprs)
+        {
+            // Check if the entire expr came from an argument
+            auto L = SM.getSpellingLoc(Ex->getBeginLoc());
+            if (std::any_of(
+                    Expansion->Arguments.begin(),
+                    Expansion->Arguments.end(),
+                    locInArg(L)))
+                continue;
+
+            // If not, the expression may still be safe if the L-value operand
+            // does not contain any subtree that came from an argument
+            const clang::Expr *Operand = nullptr;
+            if (auto AO = clang::dyn_cast<clang::UnaryOperator>(Ex))
+                Operand = AO->getSubExpr();
+            else if (auto BO = clang::dyn_cast<clang::BinaryOperator>(Ex))
+                Operand = BO->getLHS();
+            else
+                assert(!"Matched a non &, non binary assignment operator");
+
+            for (auto Arg : Expansion->Arguments)
+                if (isInTree(
+                        Operand,
+                        [&SM, &Arg](const clang::Stmt *ST)
+                        {
+                            auto L = SM.getSpellingLoc(ST->getBeginLoc());
+                            return locInArg(L)(Arg);
+                        }))
+                    return false;
+        }
+        return true;
+    }
+
     void Cpp2CASTConsumer::HandleTranslationUnit(clang::ASTContext &Ctx)
     {
         auto &SM = Ctx.getSourceManager();
-        const auto &LO = Ctx.getLangOpts();
         for (auto TLE : MF->TopLevelExpansions)
         {
             using namespace clang::ast_matchers;
@@ -106,7 +296,7 @@ namespace cpp2c
 
             //// Print macro expansion info
 
-            // TLE->dumpASTInfo(llvm::errs(), SM, LO);
+            // TLE->dumpASTInfo(llvm::errs(), SM, Ctx.getLangOpts());
 
             if (!TLE->AlignedRoot)
                 llvm::errs() << "Unaligned body,";
@@ -179,6 +369,33 @@ namespace cpp2c
                 llvm::errs() << "Aligned arguments,";
             else
                 llvm::errs() << "Unaligned argument,";
+
+            // Check for semantic properties of interface-equivalence
+            // TODO: Check for these properties in decls as well?
+
+            if (TLE->AlignedRoot &&
+                TLE->AlignedRoot->ST)
+            {
+                if (isHygienic(Ctx, TLE))
+                    llvm::errs() << "Hygienic,";
+                else
+                    llvm::errs() << "Unhygienic,";
+                if (isParameterSideEffectFree(Ctx, TLE))
+                    llvm::errs() << "Parameter side-effect free,";
+                else
+                    llvm::errs() << "Parameter side-effects,";
+                if (isLValueIndependent(Ctx, TLE))
+                    llvm::errs() << "L-value independent,";
+                else
+                    llvm::errs() << "L-value dependent,";
+            }
+            else
+            {
+                llvm::errs() << "Cannot check for hygiene,";
+                llvm::errs() << "Cannot check for parameter side-effects,";
+                llvm::errs() << "Cannot check for L-value independence,";
+            }
+
             llvm::errs() << "\n";
         }
     }
