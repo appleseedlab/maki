b8f00758fb1c08b44e0971ba9b8b202a7b08268c
Brent Pappas <pappasbrent@gmail.com>
2022-09-20 13:15:14 -0400

    move isInTree function into a utils file

---
+++ src/ASTUtils.cc
@@ -0,0 +1,16 @@
+#include "ASTUtils.hh"
+
+namespace cpp2c
+{
+    bool isInTree(
+        const clang::Stmt *ST,
+        std::function<bool(const clang::Stmt *)> pred)
+    {
+        if (pred(ST))
+            return true;
+        for (auto child : ST->children())
+            if (isInTree(child, pred))
+                return true;
+        return false;
+    }
+} // namespace cpp2c
---
+++ src/ASTUtils.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "clang/AST/Stmt.h"
+
+#include <functional>
+
+namespace cpp2c
+{
+    bool isInTree(
+        const clang::Stmt *ST,
+        std::function<bool(const clang::Stmt *)> pred);
+} // namespace cpp2c
