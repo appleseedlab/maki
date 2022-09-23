40e3a1cc8b14bc5c4e6b07ecef1dcf3ab3463a8e
Brent Pappas <pappasbrent@gmail.com>
2022-09-23 17:05:54 -0400

    fix typo in comment

--- tests/wellformed_arguments.c
+++ tests/wellformed_arguments.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 {
     ID(1);
     // For the following macros, any operator with a higher precedence than
-    // the ones applies to the arguments should unalign the argument
+    // the ones applied to the arguments should unalign the argument
     ADD(1 * 2, 3 * 4);
     MUL((double)1, !2);
     AND(1 | 2, 3 | 4);
