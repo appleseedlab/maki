3b64f34c4d9271e3b5107435d1fa6a248c5af811
Brent Pappas <pappasbrent@gmail.com>
2022-09-23 17:04:56 -0400

    improve tests, add comments

--- tests/wellformed_arguments.c
+++ tests/wellformed_arguments.c
@@ -1,16 +1,16 @@
 #define ID(x) x
-#define ADD(a, b) ((a) + (b))
-#define MUL(a, b) ((a) * (b))
-#define AND(a, b) ((a) && (b))
+#define ADD(a, b) a + b
+#define MUL(a, b) a * b
+#define AND(a, b) a && b
 
 int main(int argc, char const *argv[])
 {
-    int x = 0, y = 2;
     ID(1);
-    ADD(x, y);
-    MUL((1 + 2), (3 * 4 / 5 << 6 % 7));
-    AND((1 + 2 - 3 * 4 / 5), (6 << 7 >> 8 % 9 & 10 | 11));
-    ADD(1, 2);
+    // For the following macros, any operator with a higher precedence than
+    // the ones applies to the arguments should unalign the argument
+    ADD(1 * 2, 3 * 4);
+    MUL((double)1, !2);
+    AND(1 | 2, 3 | 4);
 
     return 0;
 }
