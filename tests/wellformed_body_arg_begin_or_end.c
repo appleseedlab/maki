733d6d1d2002c06f4108bdf6e8d3d6cb00bee1a7
Brent Pappas <pappasbrent@gmail.com>
2022-09-24 14:46:45 -0400

    add test for finding wellformed macros whose bodies begin/end with a macro argument

---
+++ tests/wellformed_body_arg_begin_or_end.c
@@ -0,0 +1,26 @@
+#define ID(x) x
+
+/*
+        +
+    x       y
+*/
+#define ADD(x, y) x + y
+
+/*
+            +
+        x           +
+                y       +
+                    x       y
+*/
+#define FOO(x, y) x + y + x + y
+
+int main(int argc, char const *argv[])
+{
+    ID(1);
+    ADD(1, 2);
+    ADD(1 * 2, 3 * 4);
+    FOO(1, 2);
+    FOO(1 * 2, 3 * 4);
+
+    return 0;
+}
