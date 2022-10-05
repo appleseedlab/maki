411644084c8e981f39cbfb7742c7147398d50e4e
Brent Pappas <pappasbrent@gmail.com>
2022-10-05 15:40:38 -0400

    fix typo in test
    change ADDR_OFF to be ADDR_OF_G

--- tests/l_value_independent.c
+++ tests/l_value_independent.c
@@ -1,4 +1,4 @@
-#define ADDR_OFF(x) (&(g))
+#define ADDR_OF_G (&(g))
 #define DEREF(x) (*(x))
 
 int g = 0;
@@ -9,7 +9,7 @@ int g = 0;
 
 int main(int argc, char const *argv[])
 {
-    ADDR_OFF(g);
+    ADDR_OF_G;
     int x = 0;
     DEREF(&x);
 
