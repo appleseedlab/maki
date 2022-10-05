96907a4ea41f62e32150b6b067891db6f1e9f497
Brent Pappas <pappasbrent@gmail.com>
2022-10-05 15:40:11 -0400

    add test for expansions that contain anonymous types

---
+++ tests/anonymous_types.c
@@ -0,0 +1,55 @@
+typedef struct
+{
+    char *name;
+    unsigned int age;
+} Person;
+
+#define PARENT_GET_AGE(P) ((P).age)
+#define PARENT_PTR_GET_AGE(P) ((P)->age)
+
+enum COLOR_TYPE
+{
+    RGB,
+    HSL
+};
+
+struct color_t
+{
+    enum COLOR_TYPE ty;
+    union
+    {
+        struct
+        {
+            unsigned int r, g, b;
+        } rgb;
+        struct
+        {
+            unsigned int h, s, l;
+        } hsl;
+    };
+};
+
+#define RGB_GET_R(rgb) ((rgb).r)
+#define RGB_PTR_GET_R(rgb) ((rgb)->r)
+
+int main(int argc, char const *argv[])
+{
+    Person p;
+    p.age = 30;
+    // Anonymous types
+    PARENT_GET_AGE(p);
+    // Anonymous types
+    PARENT_PTR_GET_AGE(&p);
+
+    struct color_t c;
+    c.ty = RGB;
+    c.rgb.r = 0x255;
+    c.rgb.g = 0x255;
+    c.rgb.b = 0;
+
+    RGB_GET_R(c.rgb);
+    // Anonymous types
+    RGB_PTR_GET_R(&c.rgb);
+
+    return 0;
+}
