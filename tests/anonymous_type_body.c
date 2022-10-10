f436746779a9172eb0689a709cb1706e5594362d
Brent Pappas <pappasbrent@gmail.com>
2022-10-10 19:22:31 -0400

    add test for expansion expanding to an expression having an anonymous type

---
+++ tests/anonymous_type_body.c
@@ -0,0 +1,49 @@
+typedef struct
+{
+    char *name;
+    unsigned int age;
+} Person;
+
+#define ID(P) P
+#define PTR(P) (&(P))
+
+enum COLOR_TYPE
+{
+    RGB,
+    HSL
+};
+
+struct color_t
+{
+    enum COLOR_TYPE ty;
+    union
+    {
+        struct
+        {
+            unsigned int r, g, b;
+        } rgb;
+        struct
+        {
+            unsigned int h, s, l;
+        } hsl;
+    };
+};
+
+#define COLOR_GET_RGB(color) ((color).rgb)
+#define COLOR_GET_RGB_PTR(color) ((color)->rgb)
+
+int main(int argc, char const *argv[])
+{
+    Person p;
+    // Anonymous types
+    ID(p);
+    // Anonymous types
+    PTR(p);
+
+    struct color_t c;
+    COLOR_GET_RGB(c);
+    // Anonymous types
+    COLOR_GET_RGB_PTR(&c);
+
+    return 0;
+}
