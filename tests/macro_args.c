90174da1c845b8353f2e93141dc741ea48224f80
Brent Pappas <pappasbrent@gmail.com>
2022-10-17 14:13:32 -0400

    add tests for checking that aligned macros passed to other aligned macros as arguments are aligned

--- tests/macro_args.c
+++ tests/macro_args.c
@@ -1,8 +1,27 @@
+#define FOO(a, b) a + b + a + b
+
+#define ID(a) a
+#define PAREN(a) (a)
 #define ADD(a, b) ((a) + (b))
 #define SUB(a, b) ((a) - (b))
 
+#define MULT_MALFORMED(a, b) a *b
+#define ADD_MALFORMED(a, b) a + b
+
 int main(int argc, char const *argv[])
 {
+    // This is a hard test
+    // All these should have aligned bodies and arguments
+
+    // TODO: Test more; I feel like I can still break this
+
+    FOO(1, 2);
+    FOO(1 * 2, 3 * 4);
+
+    ID(ID(42));
+
+    PAREN(PAREN(1));
+
     ADD(ADD(1, 2), 3);
 
     ADD(1, ADD(2, 3));
@@ -14,6 +33,12 @@ int main(int argc, char const *argv[])
     ADD(1, SUB(2, 3));
 
     ADD(SUB(1, 2), SUB(3, 4));
+
+    // Might be tricky
+    ADD(ADD(ADD(1, 2), ADD(3, 4)), SUB(ADD(5, 6), 7));
+
+    // Aligned body, unaligned arguments
+    MULT_MALFORMED(ADD_MALFORMED(1, 2), ADD_MALFORMED(3, 4));
 
     return 0;
 }
