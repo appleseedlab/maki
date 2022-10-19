0a24d6ae977f1619dfa3188973b30ae678b025d1
Brent Pappas <pappasbrent@gmail.com>
2022-10-19 15:47:45 -0400

    add a test for arbitrarily deeply nested macro invocations

--- tests/nested_macros.c
+++ tests/nested_macros.c
@@ -5,6 +5,13 @@
 #define FOUR (ADD(TWO, TWO))
 #define FIVE (ADD(TWO, FOUR))
 
+// Test matching of simple, but arbitrarily deeply nested,
+// macro invocations
+#define L1 L2
+#define L2 L3
+#define L3 L4
+#define L4 1
+
 // tests multiple invocations in a single argument
 #define THREE_X (ADD(ONE + ONE, TWO))
 
@@ -26,6 +33,8 @@ int main(int argc, char const *argv[])
     THREE_X;
     FOUR_X;
     FIVE_X;
+
+    L1;
 
     return 0;
 }
