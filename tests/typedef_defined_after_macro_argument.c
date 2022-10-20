8cb6a2766539c2a2ca1680202b8a8772177c00ac
Brent Pappas <pappasbrent@gmail.com>
2022-10-20 15:02:30 -0400

    add tests for whether a macro argument is of a type containing a typedef deifned after the macro itself was defined

---
+++ tests/typedef_defined_after_macro_argument.c
@@ -0,0 +1,48 @@
+#define F(a) (a).x
+#define G(a) (*(a)).x
+#define H(a) (a[0]).x
+#define I(i) ((Int)i)
+
+typedef struct A
+{
+    int x;
+} A;
+
+typedef int Int;
+
+int main(int argc, char const *argv[])
+{
+    A a;
+    A *b = &a;
+    A c[3];
+
+    // Typedef (type) defined after macro argument
+    F(a);
+    // Typedef (type) defined after macro argument
+    G(b);
+    // Typedef (type) defined after macro argument
+    H(c);
+    // Typedef defined after macro argument
+    I(1);
+
+#undef F
+#undef G
+#undef H
+#undef I
+
+#define F(a) (a).x
+#define G(a) (*(a)).x
+#define H(a) (a[0]).x
+#define I(i) ((Int)i)
+
+    // Typedef (type) defined before macro argument
+    F(a);
+    // Typedef (type) defined before macro argument
+    G(b);
+    // Typedef (type) defined before macro argument
+    H(c);
+    // Typedef defined before macro argument
+    I(1);
+
+    return 0;
+}
