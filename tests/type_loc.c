d08eb909641fcf5882e22ecf5c2166e8fa18c6fc
Brent Pappas <pappasbrent@gmail.com>
2022-10-24 13:59:01 -0400

    improve tests for TypeLoc expansions

--- tests/type_loc.c
+++ tests/type_loc.c
@@ -1,6 +1,29 @@
-#define INT int
+// Should all have a single aligned body
 
-INT x;
+#define CHAR char
+
+#define UINT unsigned int
+
+#define LL long long
+
+#define ULL unsigned long long
+
+CHAR a;
+UINT b;
+LL c;
+ULL d;
+
+CHAR f(ULL x, UINT *c);
+
+struct X
+{
+    ULL a[256];
+    union
+    {
+        CHAR c;
+        LL b;
+    };
+};
 
 int main(int argc, char const *argv[])
 {
