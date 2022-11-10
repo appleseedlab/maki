f48614c01b18cdf718eea70a01f2c0e7f80012cb
Brent Pappas <pappasbrent@gmail.com>
2022-11-10 14:15:34 -0500

    Make sure that we don't visit null Stmts

--- src/ASTUtils.cc
+++ src/ASTUtils.cc
@@ -6,11 +6,16 @@ namespace cpp2c
         const clang::Stmt *ST,
         std::function<bool(const clang::Stmt *)> pred)
     {
+        if (!ST)
+            return false;
+
         if (pred(ST))
             return true;
-        for (auto child : ST->children())
+
+        for (auto &&child : ST->children())
             if (isInTree(child, pred))
                 return true;
+
         return false;
     }
 } // namespace cpp2c
