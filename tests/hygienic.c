10696769dc8f32c968d5cf29d0a36a3c4b345469
Brent Pappas <pappasbrent@gmail.com>
2022-12-09 13:05:04 -0500

    add test that variables local to macro expansions are considered hygienic

--- tests/hygienic.c
+++ tests/hygienic.c
@@ -4,11 +4,17 @@ int g = 0;
 
 #define GT_g(x) ((x) > (g))
 
+#define DO_NOTHING do { int x = 0; x++; } while (0)
+
 int main(int argc, char const *argv[])
 {
     int x = 0, y = 1;
     MAX(x, y);
     MAX(x, g);
     GT_g(x);
+
+    // Hygienic since the only variable it references is declared within
+    // the expanded code itself
+    DO_NOTHING;
     return 0;
 }
