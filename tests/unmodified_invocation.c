8efcb240067c2a9808ef9d452344919798883db4
Brent Pappas <pappasbrent@gmail.com>
2022-12-27 17:42:12 -0500

    add comments

--- tests/unmodified_invocation.c
+++ tests/unmodified_invocation.c
@@ -1,13 +1,17 @@
+// The bodies of all invocations are not modified values
+
 #define INC_G g++
 
 int g = 0;
 
 int main(int argc, char const *argv[])
 {
+    // Impure, but not expanded where a modifiable value is required
     INC_G;
 
     {
         int g = 0;
+        // Impure, but not expanded where a modifiable value is required
         INC_G;
     }
 
