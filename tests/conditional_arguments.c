7ebc7d83cc98273e2436eb309c19218960f91f8c
Brent Pappas <pappasbrent@gmail.com>
2022-12-28 10:07:36 -0500

    add tests for conditionally evaluated arguments

---
+++ tests/conditional_arguments.c
@@ -0,0 +1,15 @@
+// All macro invocations have an argument that is conditionally evaluated
+
+#define ONE_AND_A(A) ((1) && (A))
+#define A_AND_ONE(A) ((A) && (1))
+
+#define ZERO(A) (1 ? (0) : (p))
+
+int main(int argc, char const *argv[])
+{
+    int *p = ((void *)0);
+    ONE_AND_A(*p);
+    A_AND_ONE(*p);
+    ZERO(*p);
+    return 0;
+}
