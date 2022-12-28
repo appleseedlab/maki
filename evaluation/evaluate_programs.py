7dde68a4e0c8fd1e67cbfa261c4f002c1d5d06fe
Brent Pappas <pappasbrent@gmail.com>
2022-12-28 11:01:56 -0500

    add script to evaluate all programs

---
+++ evaluation/evaluate_programs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from datetime import datetime
+from subprocess import run
+
+from evaluation_programs import PROGRAMS
+
+USAGE_STRING = r"./check_program.py CPP2C_SO_PATH"
+
+
+def main():
+
+    # check that the user passed the path to the cpp2c shared object file
+    if len(sys.argv) != 2:
+        print(USAGE_STRING)
+        exit(1)
+
+    # extract the cpp2c so path from the command line arguments
+    _, cpp2c_so_path = sys.argv
+
+    # create the results directory
+    os.makedirs('./results/', exist_ok=True)
+
+    # evaluate each program
+    for p in PROGRAMS:
+        if not os.path.exists(p.extracted_archive_path):
+            print(f"warning: skipping {p.name}, directory not found")
+            continue
+
+        src_dir = p.extracted_archive_path + '/' + p.src_dir
+        dst_dir = f"./results/{p.name}"
+        cmd = ' '.join(["./check_program.py ",
+                       cpp2c_so_path,
+                       p.extracted_archive_path,
+                       src_dir,
+                       dst_dir])
+        print(cmd)
+        t0 = datetime.now()
+        run(cmd, shell=True, capture_output=True).check_returncode()
+        t1 = datetime.now()
+        evaluation_time = t1 - t0
+        print(f'evaluation time: {evaluation_time}')
+
+
+if __name__ == '__main__':
+    main()
