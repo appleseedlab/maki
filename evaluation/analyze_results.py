e9d8ec114e977d1d56a0376c2dd39e429c4e18a0
Brent Pappas <pappasbrent@gmail.com>
2023-01-20 14:42:54 -0500

    reorganize analysis code and collect information on applicable transformations for macro definitions and invocations

--- evaluation/analyze_results.py
+++ evaluation/analyze_results.py
@@ -2,87 +2,60 @@
 
 import json
 import sys
-from collections import defaultdict
-from dataclasses import dataclass
-from typing import Dict, Set
+from dataclasses import asdict
+from typing import Callable
 
+from analysis import Analysis, definition_stat, invocation_stat
+from macros import Invocation, Macro, PreprocessorData
+from predicates.argument_altering import aa_invocation
+from predicates.declaration_altering import da_invocation
+from predicates.call_site_context_altering import csca_invocation
+from predicates.interface_equivalent import ie_def
+from predicates.metaprogramming import mp_invocation
+from predicates.thunkizing import thunkizing_invocation
+
+ANALYSES_DIR = r'ANALYSES'
+DELIM = '\t'
 USAGE_STRING = r'./analyze_results.py RESULTS_FILE'
 
-DELIM = '\t'
+
+def only(i: Invocation,
+         pd: PreprocessorData,
+         t: Callable[[Invocation, PreprocessorData], bool]):
+    transformations = [aa_invocation, da_invocation,
+                       csca_invocation, mp_invocation,
+                       thunkizing_invocation]
+    '''
+    Returns true if the transformation t is the only one that works
+    for this invocation.
+    '''
+    assert t in transformations
+    trs = [tr for tr in transformations if tr(i, pd)]
+    return trs == [t]
 
 
-@dataclass(frozen=True)
-class Macro:
-    Name: str
-    ValidDefLoc: bool
-    DefLocOrError: str
+def mdefs_only_t(pd, t):
+    return definition_stat(
+        pd,
+        lambda m, pd: all([only(i, pd, t) for i in pd.mm[m]]))
 
 
-@dataclass(frozen=True)
-class Invocation:
-    Name: str
-    DefinitionLocation: str
-    InvocationLocation: str
-    ASTKind: str
-    TypeSignature: str
-
-    InvocationDepth: int
-    NumASTRoots: int
-    NumArguments: int
-
-    HasStringification: bool
-    HasTokenPasting: bool
-    HasAlignedArguments: bool
-    HasSameNameAsOtherDeclaration: bool
-
-    IsExpansionControlFlowStmt: bool
-
-    DoesBodyReferenceMacroDefinedAfterMacro: bool
-    DoesBodyReferenceDeclDeclaredAfterMacro: bool
-    DoesBodyContainDeclRefExpr: bool
-    DoesSubexpressionExpandedFromBodyHaveLocalType: bool
-    DoesSubexpressionExpandedFromBodyHaveTypeDefinedAfterMacro: bool
-
-    DoesAnyArgumentHaveSideEffects: bool
-    DoesAnyArgumentContainDeclRefExpr: bool
-
-    IsHygienic: bool
-    IsDefinitionLocationValid: bool
-    IsInvocationLocationValid: bool
-    IsObjectLike: bool
-    IsInvokedInMacroArgument: bool
-    IsNamePresentInCPPConditional: bool
-    IsExpansionICE: bool
-
-    IsExpansionTypeNull: bool
-    IsExpansionTypeAnonymous: bool
-    IsExpansionTypeLocalType: bool
-    IsExpansionTypeDefinedAfterMacro: bool
-    IsExpansionTypeVoid: bool
-
-    IsAnyArgumentTypeNull: bool
-    IsAnyArgumentTypeAnonymous: bool
-    IsAnyArgumentTypeLocalType: bool
-    IsAnyArgumentTypeDefinedAfterMacro: bool
-    IsAnyArgumentTypeVoid: bool
-
-    IsInvokedWhereModifiableValueRequired: bool
-    IsInvokedWhereAddressableValueRequired: bool
-    IsInvokedWhereICERequired: bool
-
-    IsAnyArgumentExpandedWhereModifiableValueRequired: bool
-    IsAnyArgumentExpandedWhereAddressableValueRequired: bool
-    IsAnyArgumentConditionallyEvaluated: bool
-    IsAnyArgumentNeverExpanded: bool
-    IsAnyArgumentNotAnExpression: bool
+def mdefs_at_least_t(pd, t):
+    return definition_stat(pd,
+                           lambda m, pd: any([t(i, pd) for i in pd.mm[m]]))
 
 
-MacroMap = Dict[Macro, Set[Invocation]]
+def invocations_only_t(pd, t):
+    return invocation_stat(pd, lambda i, pd_: only(i, pd_, t))
+
+
+def invocations_at_least_t(pd, t):
+    return invocation_stat(pd, t)
 
 
 def main():
     if len(sys.argv) != 2:
-        print(USAGE_STRING)
+        print(USAGE_STRING, file=sys.stderr)
         exit(1)
     f = sys.argv[1]
 
@@ -90,12 +63,7 @@ def main():
     with open(f) as fp:
         lines = fp.readlines()
 
-    # mapping from macros to invocations
-    mis: MacroMap = defaultdict(set)
-    # set of names of macros inspected by the CPP
-    inspected_macro_names = set()
-    # set of files included at a local scope
-    local_includes = set()
+    pd = PreprocessorData()
 
     # src directory, to be initialized during the analysis
     src_dir = ''
@@ -107,69 +75,132 @@ def main():
             _, src_dir = line.split(DELIM)
 
         elif line.startswith('Define'):
-            _, Name, ValidDefLoc, DefLocOrError = line.split(DELIM)
-            m = Macro(Name, ValidDefLoc == 'T', DefLocOrError)
-            if m not in mis:
-                mis[m] = set()
+            _, Name, IsObjectLike, IsDefLocValid, DefLocOrError = line.split(
+                DELIM)
+            m = Macro(Name, IsObjectLike == 'T',
+                      IsDefLocValid == 'T', DefLocOrError)
+            if m not in pd.mm:
+                pd.mm[m] = set()
 
         elif line.startswith('InspectedByCPP'):
             _, Name = line.split(DELIM)
-            inspected_macro_names.add(Name)
+            pd.inspected_macro_names.add(Name)
 
         elif line.startswith('Include'):
             _, Valid, IncludedFileRealPath = line.split(DELIM)
             if Valid == 'F':
-                local_includes.add(IncludedFileRealPath)
+                pd.local_includes.add(IncludedFileRealPath)
 
         elif line.startswith('Invocation'):
             _, j = line.split(DELIM, 1)
             i = Invocation(**(json.loads(j)))
             m = Macro(i.Name,
+                      i.IsObjectLike,
                       i.IsInvocationLocationValid,
                       i.DefinitionLocation)
-            mis[m].add(i)
+            # Only record unique invocations - two invocations may have the same
+            # location if they are the same nested invocation
+            if all([j.InvocationLocation != i.InvocationLocation for j in pd.mm[m]]):
+                pd.mm[m].add(i)
 
-    print('defined macros:', len(mis.keys()))
+    # src_pd only records preprocessor data about source macros
+    src_pd = PreprocessorData(
+        {m: is_ for m, is_ in pd.mm.items() if m.defined_in(src_dir)},
+        pd.inspected_macro_names,
+        pd.local_includes
+    )
 
-    print('macros defined at invalid locations',
-          len([m for m in mis.keys() if not m.ValidDefLoc]))
+    a = Analysis(
+        defined_macros=definition_stat(pd, lambda _m, _pd: True),
+        macros_defined_at_valid_src_locs=definition_stat(
+            src_pd, lambda m, _pd: True),
 
-    src_mis = {m: is_ for m, is_ in mis.items()
-               if m.DefLocOrError.startswith(src_dir)}
+        # Can just return true because we only record invocations at unique
+        # locations anyway
+        src_invocations_at_unique_locations=invocation_stat(
+            src_pd, lambda _i, _pd: True),
+        src_invocations_at_unique_valid_locations=invocation_stat(
+            src_pd, lambda i, _pd: i.IsInvocationLocationValid),
+        src_invocations_at_unique_invalid_locations=invocation_stat(
+            src_pd, lambda i, _pd: not i.IsInvocationLocationValid),
 
-    print('macros defined in src_dir', len(src_mis))
+        nested_argument_src_invocations=invocation_stat(
+            src_pd,
+            lambda i, _pd: i.InvocationDepth > 0 and i.IsInvokedInMacroArgument),
+        nested_non_argument_src_invocations=invocation_stat(
+            src_pd,
+            lambda i, _pd: i.InvocationDepth > 0 and not i.IsInvokedInMacroArgument),
+        toplevel_argument_src_invocations=invocation_stat(
+            src_pd,
+            lambda i, _pd: i.InvocationDepth == 0 and i.IsInvokedInMacroArgument),
+        toplevel_non_argument_src_invocations=invocation_stat(
+            src_pd,
+            lambda i, _pd: i.InvocationDepth == 0 and not i.IsInvokedInMacroArgument),
 
-    print('number of unique src macro invocations',
-          sum([len(is_) for is_ in src_mis.values()]))
+        aligned_src_invocations=invocation_stat(
+            src_pd, lambda i, _pd: i.HasValidSemanticData and i.IsAligned),
 
-    print('src invocations at invalid locations',
-          sum([len([i for i in is_ if not i.IsInvocationLocationValid])
-               for is_ in src_mis.values()]))
+        interface_equivalent_src_definitions=definition_stat(src_pd, ie_def),
+        # We have to compute the invocation stat for
+        # interface-equivalent macro invocations somewhat differently
+        # from the rest because in order for an invocation to truly
+        # be interface-equivalent, all its definition's corresponding
+        # invocations must be interface-equivalent as well
+        interface_equivalent_src_invocations=invocation_stat(
+            PreprocessorData({m: is_ for m, is_ in src_pd.mm.items()
+                              if ie_def(m, src_pd)},
+                             src_pd.inspected_macro_names,
+                             src_pd.local_includes),
+            lambda _m, _pd: True),
 
-    print('nested src invocations',
-          sum([len([i for i in is_ if i.InvocationDepth > 0])
-               for is_ in src_mis.values()]))
+        src_definitions_with_only_argument_altering_invocations=mdefs_only_t(
+            src_pd, aa_invocation),
+        src_definitions_with_any_argument_altering_invocations=mdefs_at_least_t(
+            src_pd, aa_invocation),
+        toplevel_non_argument_src_invocations_that_are_only_argument_altering=invocations_only_t(
+            src_pd, aa_invocation),
+        toplevel_non_argument_src_invocations_that_are_at_least_argument_altering=invocations_at_least_t(
+            src_pd, aa_invocation),
 
-    print('src invocations in macro arguments',
-          sum([len([i for i in is_ if i.IsInvokedInMacroArgument])
-               for is_ in src_mis.values()]))
+        src_definitions_with_only_declaration_altering_invocations=mdefs_only_t(
+            src_pd, da_invocation),
+        src_definitions_with_any_declaration_altering_invocations=mdefs_at_least_t(
+            src_pd, da_invocation),
+        toplevel_non_argument_src_invocations_that_are_only_declaration_altering=invocations_only_t(
+            src_pd, da_invocation),
+        toplevel_non_argument_src_invocations_that_are_at_least_declaration_altering=invocations_at_least_t(
+            src_pd, da_invocation),
 
-    print('aligned src invocations',
-          sum([len([i for i in is_
-                    if all([i.NumASTRoots == 1,
-                           i.HasAlignedArguments])])
-               for is_ in src_mis.values()]))
+        src_definitions_with_only_call_site_context_altering_invocations=mdefs_only_t(
+            src_pd, csca_invocation),
+        src_definitions_with_any_call_site_context_altering_invocations=mdefs_at_least_t(
+            src_pd, csca_invocation),
+        toplevel_non_argument_src_invocations_that_are_only_call_site_context_altering=invocations_only_t(
+            src_pd, csca_invocation),
+        toplevel_non_argument_src_invocations_that_are_at_least_call_site_context_altering=invocations_at_least_t(
+            src_pd, csca_invocation),
 
-    # TODO: double check that these are actually the minimum requirements for
-    # transformation
+        src_definitions_with_only_thunkizing_invocations=mdefs_only_t(
+            src_pd, thunkizing_invocation),
+        src_definitions_with_any_thunkizing_invocations=mdefs_at_least_t(
+            src_pd, thunkizing_invocation),
+        toplevel_non_argument_src_invocations_that_are_only_thunkizing=invocations_only_t(
+            src_pd, thunkizing_invocation),
+        toplevel_non_argument_src_invocations_that_are_at_least_thunkizing=invocations_at_least_t(
+            src_pd, thunkizing_invocation),
 
-    print('transformable src invocations',
-          sum([len([i for i in is_
-                    if all([i.NumASTRoots == 1,
-                           i.HasAlignedArguments,
-                           not i.IsAnyArgumentNotAnExpression,
-                           not i.IsExpansionControlFlowStmt])])
-               for is_ in src_mis.values()]))
+        src_definitions_with_only_metaprogramming_invocations=mdefs_only_t(
+            src_pd, mp_invocation),
+        src_definitions_with_any_metaprogramming_invocations=mdefs_at_least_t(
+            src_pd, mp_invocation),
+        toplevel_non_argument_src_invocations_that_are_only_metaprogramming=invocations_only_t(
+            src_pd, mp_invocation),
+        toplevel_non_argument_src_invocations_that_are_at_least_metaprogramming=invocations_at_least_t(
+            src_pd, mp_invocation),
+
+    )
+
+    json.dump(asdict(a), sys.stdout, indent=4)
 
 
 if __name__ == '__main__':
