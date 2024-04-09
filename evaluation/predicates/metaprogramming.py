a8a8925bd78ff4dfd04b2917c7c1e2968501b401
Brent Pappas <pappasbrent@gmail.com>
2024-04-09 12:58:31 -0400

    Remove artifact evaluation code

--- evaluation/predicates/metaprogramming.py
+++
@@ -1,6 +0,0 @@
-from macros import Invocation, PreprocessorData
-
-
-def mp_invocation(i: Invocation, pd: PreprocessorData) -> bool:
-    assert i.IsTopLevelNonArgument
-    return i.MustUseMetaprogrammingToTransform
