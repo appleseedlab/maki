36f2bbde297776a1ff63f2664ca98dec5dd896f8
Brent Pappas <pappasbrent@gmail.com>
2024-05-15 12:23:48 -0400

    Add new test for token separation

---
+++ test/Tests/token_separation.c
@@ -0,0 +1,95 @@
+// RUN: maki %s | jq '[.[] | select(.IsDefinitionLocationValid == null or .IsDefinitionLocationValid == true)] | sort_by(.PropertiesOf, .DefinitionLocation, .InvocationLocation)' | FileCheck %s --color
+
+// COM: We don't want to keep backslashes and newlines
+#define MULTI_LINE a \
+b \
+            c
+// COM: If we don't separate these tokens correctly, then we'd break this
+// typecast
+#define CAST(c) ((unsigned int)(unsigned char)(c))
+// COM: Empty bodies shouldn't cause problems
+#define EMPTY
+
+int main(void) {
+    int c = 0;
+    CAST(c);
+    return 0;
+}
+
+// CHECK: [
+// CHECK:   {
+// CHECK:     "Kind": "Definition",
+// CHECK:     "Name": "EMPTY",
+// CHECK:     "IsObjectLike": true,
+// CHECK:     "IsDefinitionLocationValid": true,
+// CHECK:     "Body": "",
+// CHECK:     "DefinitionLocation": "{{.*}}/Tests/token_separation.c:11:9",
+// CHECK:     "EndDefinitionLocation": "{{.*}}/Tests/token_separation.c:11:9"
+// CHECK:   },
+// CHECK:   {
+// CHECK:     "Kind": "Definition",
+// CHECK:     "Name": "MULTI_LINE",
+// CHECK:     "IsObjectLike": true,
+// CHECK:     "IsDefinitionLocationValid": true,
+// CHECK:     "Body": "a b c",
+// CHECK:     "DefinitionLocation": "{{.*}}/Tests/token_separation.c:4:9",
+// CHECK:     "EndDefinitionLocation": "{{.*}}/Tests/token_separation.c:6:13"
+// CHECK:   },
+// CHECK:   {
+// CHECK:     "Kind": "Definition",
+// CHECK:     "Name": "CAST",
+// CHECK:     "IsObjectLike": false,
+// CHECK:     "IsDefinitionLocationValid": true,
+// CHECK:     "Body": "( ( unsigned int ) ( unsigned char ) ( c ) )",
+// CHECK:     "DefinitionLocation": "{{.*}}/Tests/token_separation.c:9:9",
+// CHECK:     "EndDefinitionLocation": "{{.*}}/Tests/token_separation.c:9:50"
+// CHECK:   },
+// CHECK:   {
+// CHECK:     "Kind": "Invocation",
+// CHECK:     "Name": "CAST",
+// CHECK:     "DefinitionLocation": "{{.*}}/Tests/token_separation.c:9:9",
+// CHECK:     "InvocationLocation": "{{.*}}/Tests/token_separation.c:15:5",
+// CHECK:     "ASTKind": "Expr",
+// CHECK:     "TypeSignature": "unsigned int CAST(int c)",
+// CHECK:     "InvocationDepth": 0,
+// CHECK:     "NumASTRoots": 1,
+// CHECK:     "NumArguments": 1,
+// CHECK:     "HasStringification": false,
+// CHECK:     "HasTokenPasting": false,
+// CHECK:     "HasAlignedArguments": true,
+// CHECK:     "HasSameNameAsOtherDeclaration": false,
+// CHECK:     "IsExpansionControlFlowStmt": false,
+// CHECK:     "DoesBodyReferenceMacroDefinedAfterMacro": false,
+// CHECK:     "DoesBodyReferenceDeclDeclaredAfterMacro": false,
+// CHECK:     "DoesBodyContainDeclRefExpr": false,
+// CHECK:     "DoesSubexpressionExpandedFromBodyHaveLocalType": false,
+// CHECK:     "DoesSubexpressionExpandedFromBodyHaveTypeDefinedAfterMacro": false,
+// CHECK:     "DoesAnyArgumentHaveSideEffects": false,
+// CHECK:     "DoesAnyArgumentContainDeclRefExpr": true,
+// CHECK:     "IsHygienic": true,
+// CHECK:     "IsDefinitionLocationValid": true,
+// CHECK:     "IsInvocationLocationValid": true,
+// CHECK:     "IsObjectLike": false,
+// CHECK:     "IsInvokedInMacroArgument": false,
+// CHECK:     "IsNamePresentInCPPConditional": false,
+// CHECK:     "IsExpansionICE": false,
+// CHECK:     "IsExpansionTypeNull": false,
+// CHECK:     "IsExpansionTypeAnonymous": false,
+// CHECK:     "IsExpansionTypeLocalType": false,
+// CHECK:     "IsExpansionTypeDefinedAfterMacro": false,
+// CHECK:     "IsExpansionTypeVoid": false,
+// CHECK:     "IsAnyArgumentTypeNull": false,
+// CHECK:     "IsAnyArgumentTypeAnonymous": false,
+// CHECK:     "IsAnyArgumentTypeLocalType": false,
+// CHECK:     "IsAnyArgumentTypeDefinedAfterMacro": false,
+// CHECK:     "IsAnyArgumentTypeVoid": false,
+// CHECK:     "IsInvokedWhereModifiableValueRequired": false,
+// CHECK:     "IsInvokedWhereAddressableValueRequired": false,
+// CHECK:     "IsInvokedWhereICERequired": false,
+// CHECK:     "IsAnyArgumentExpandedWhereModifiableValueRequired": false,
+// CHECK:     "IsAnyArgumentExpandedWhereAddressableValueRequired": false,
+// CHECK:     "IsAnyArgumentConditionallyEvaluated": false,
+// CHECK:     "IsAnyArgumentNeverExpanded": false,
+// CHECK:     "IsAnyArgumentNotAnExpression": false
+// CHECK:   }
+// CHECK: ]
