8a30d95767f049d997c174288e17d1ded520b183
Brent Pappas <pappasbrent@gmail.com>
2024-05-20 09:46:34 -0400

    Review includes, add curly braces

--- lib/StmtCollectorMatchHandler.cc
+++ lib/StmtCollectorMatchHandler.cc
@@ -1,11 +1,14 @@
 #include "StmtCollectorMatchHandler.hh"
+#include <clang/AST/Stmt.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
 
 namespace maki {
 void StmtCollectorMatchHandler::run(
     const clang::ast_matchers::MatchFinder::MatchResult &Result) {
-    if (auto ST = Result.Nodes.getNodeAs<clang::Stmt>("root"))
+    if (auto ST = Result.Nodes.getNodeAs<clang::Stmt>("root")) {
         Stmts.insert(ST);
-    else
+    } else {
         assert(!"Matched a non-Stmt node");
+    }
 }
 } // namespace maki
