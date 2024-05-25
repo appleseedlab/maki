51fe521bf2a2b35b456e086004b99fa1d6a7975d
Brent Pappas <pappasbrent@gmail.com>
2024-05-25 13:57:11 -0400

    Add test for not printing system header macros

---
+++ test/Tests/std_includes.c
@@ -0,0 +1,16 @@
+// RUN: maki %s -fplugin-arg-maki---no-system-macros -fplugin-arg-maki---no-builtin-macros -fplugin-arg-maki---no-invalid-macros | jq 'sort_by(.Kind, .DefinitionLocation, .InvocationLocation)' | FileCheck %s --color
+
+#include <iso646.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// COM: Maki should ignore the system header macros and print an empty JSON
+// COM: array
+
+int main(void) {
+    (void)true;
+    printf("Hello, world!\n");
+    return 0;
+}
+
+// CHECK: []
